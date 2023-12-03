52417bc5128cf9c51c7f2479c244c737b9e40613
Adrian Samoticha <86920182+Adrian-Samoticha@users.noreply.github.com>
2023-12-03 19:31:48 +0100

    Initial commit

---
+++ src/main/java/net/adrian/my_first_fabric_mod/MyFirstFabricModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.adrian.my_first_fabric_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MyFirstFabricModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
