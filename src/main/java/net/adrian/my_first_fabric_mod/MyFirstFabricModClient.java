4b7b6fd472c8f379f2f09dd2b3af2e9545ebbc3d
Adrian Samoticha <86920182+Adrian-Samoticha@users.noreply.github.com>
2023-12-03 20:09:18 +0100

    add `MyFirstFabricModClient`

---
+++ src/main/java/net/adrian/my_first_fabric_mod/MyFirstFabricModClient.java
@@ -0,0 +1,10 @@
+package net.adrian.my_first_fabric_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstFabricModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
